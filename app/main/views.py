040a2fe03a9be32ed5d7d167a82e791b79205d46
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-01 16:04:16 +0300

    Added ogic to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from  requests import get_sources, get_article
+from  ..requests import get_sources, get_article
 
 #Views
 @main.route('/')
@@ -10,16 +10,16 @@ def index():
     '''
     #Getting news sources
     sources = get_sources()
-    title = f'{sources.title}'
+    title = 'Welcome to News OTG'
 
     return render_template('index.html',title = title, sources = sources)
 
 @main.route('/movie/<int:id>')
-def get_article(id):
+def articles(id):
     '''
     View movie page function that returns the movie details page and its data
     '''
     article = get_article(id)
-    title = f'{article.title}'
+    
 
-    return render_template('artticles.html',title = title,article=article)
+    return render_template('artticles.html',article=article)
