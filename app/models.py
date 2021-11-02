31e571aa34ed6622cfc9e236d14de00792e2a815
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-02 19:21:19 +0300

    Creates a styles.css files and styles the page

--- app/models.py
+++ app/models.py
@@ -13,9 +13,10 @@ class Articles:
     '''
     News Articles class to define news articles objects
     '''
-    def __init__(self,title,urlToImage,author,publishedAt,url):
+    def __init__(self,title,urlToImage,content,author,publishedAt,url):
         self.title = title
         self.urlToImage = urlToImage
+        self.content = content
         self.author = author
         self.publishedAt= publishedAt
         self.url=url
