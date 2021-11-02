00f4e67aaa6f5f8d5cf5d130781646c3fe6171d0
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-03 02:23:31 +0300

    Correct start.sh logic

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = '94d343343a714ec2b5f6a024996fe7d2'
+    SECRET_KEY='Flask WTF Secret Key'
 
 class ProdConfig(Config):
     pass
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export NEWS_API_KEY=94d343343a714ec2b5f6a024996fe7d2
-export SECRET_KEY=97ap1952an742012g774dr234
+export NEWS_API_KEY='94d343343a714ec2b5f6a024996fe7d2'
+export SECRET_KEY='Flask WTF Secret Key'
 
-python3.8 manage.py server
+
+python3 manage.py server
